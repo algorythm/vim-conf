411162701f47eecaa46d6cd969e5ad53da50d740
Anders Wiberg Olsen <andersvvolsen@gmail.com>
2016-06-27 06:57:41 +0200

    Ready for deployment.

--- simple.sh
+++ simple.sh
@@ -1,7 +1,7 @@
 mkdir ~/.vim/colors
 
 rm ~/.vimrc
-cp ~/.vim/vim_configuration.conf ~/.vimrc
+cp ~/.vim/_simple_vim.conf ~/.vimrc
 
 wget https://raw.githubusercontent.com/AlexMax/.vim/master/colors/one.vim 
 mv ~/.vim/one.vim ~/.vim/colors/one.vim
@@ -10,6 +10,8 @@ mv ~/.vim/one.vim ~/.vim/colors/one.vim
 rm ~/.vim/install.sh 
 rm ~/.vim/uninstall.sh
 rm ~/.vim/vim_configuration.conf
+rm ~/.vim/README.md
+rm -rf ~/.vim/.git
 
 echo
 echo Setup complete 
