91ce17ef5e30ae877b5a615116a4b5d839ac42c8
Anders Wiberg Olsen <awo@Anderss-MacBook-Air.local>
2016-07-26 16:45:45 +0200

    Fixed 'missing ]' error.

--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,12 @@
-if [ -f ~/.vim/autoload]; then
+if [ -f ~/.vim/autoload ]; then
     rm -rf ~/.vim/autoload
 fi
-if [ -f ~/.vim/bundle]; then
+if [ -f ~/.vim/bundle ]; then
     rm -rf ~/.vim/bundle
 fi
-if [ -f ~/.vim/colors]; then
+if [ -f ~/.vim/colors ]; then
     rm -rf ~/.vim/colors
 fi
-if [ -f ~/.vimrc]; then
+if [ -f ~/.vimrc ]; then
     rm ~/.vimrc
 fi
